dce0377f10a064290e613a75425822723157eeec
Your Semini01 <wickramasinghesemini@gmail.com>
2025-09-06 23:18:53 +0530

    Initial Commit

---
+++ iOSbookingAppFrontend/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSbookingAppFrontend
+//
+//  Created by Semini Wickramasinghe on 2025-09-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSbookingAppFrontend/iOSbookingAppFrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSbookingAppFrontendApp.swift
+//  iOSbookingAppFrontend
+//
+//  Created by Semini Wickramasinghe on 2025-09-06.
+//
+
+import SwiftUI
+
+@main
+struct iOSbookingAppFrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
